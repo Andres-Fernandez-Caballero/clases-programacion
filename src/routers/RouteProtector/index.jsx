0862baf0204ba0dcc4ed93ec7413960f0e183323
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-09-15 09:56:45 -0300

    ADD RouterProtector component

---
+++ src/routers/RouteProtector/index.jsx
@@ -0,0 +1,11 @@
+import { Outlet, Navigate, } from "react-router-dom";
+
+
+const RouteProtecor = ({isAllowed=true, redirectTo='/', children}) => {
+  // when is not allowed, redirect to the redirectTo path
+    if(!isAllowed) return <Navigate to={redirectTo} />
+
+    return children ? children : <Outlet />;
+}
+
+export default RouteProtecor;
