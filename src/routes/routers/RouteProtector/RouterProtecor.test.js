9bf279036f4183fe22874ec02564df3fa4ed50cf
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-10-08 21:21:24 -0300

    ADD test over RouterProtector Component

--- src/routes/routers/RouteProtector/RouterProtecor.test.js
+++ src/routes/routers/RouteProtector/RouterProtecor.test.js
@@ -1,7 +1,37 @@
 // make a test over the RouterProtector component
 
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import RouteProtecor from "./RouterProtector";
+
 describe('RouterProtector unit test', () => {
-	it('should protect a route the RouterProtector component', () => {
-		// TODO: make a test over the RouterProtector component
+	it('should protect a route rediret to indicate page', () => {
+		render(
+			<MemoryRouter initialEntries={['/']} >
+				<Routes>
+					<Route path="/" element={<h1>index</h1>} />
+					<Route element={<RouteProtecor isAllowed={false} redirectTo='/' />} >
+						<Route path="/protected-route" element={<h1>Protected Route</h1>} />
+					</Route>
+				</Routes>
+			</MemoryRouter >
+		)
+
+		expect(screen.getByText('index')).toBeInTheDocument();
+	});
+
+	it('should allow access to a route', () => {
+		render(
+			<MemoryRouter initialEntries={['/protected-route']} >
+				<Routes>
+					<Route path="/" element={<h1>index</h1>} />
+					<Route element={<RouteProtecor isAllowed={true} redirectTo='/' />} >
+						<Route path="/protected-route" element={<h1>Protected Route</h1>} />
+					</Route>
+				</Routes>
+			</MemoryRouter >
+		)
+
+		expect(screen.getByText('Protected Route')).toBeInTheDocument();
 	});
 });
