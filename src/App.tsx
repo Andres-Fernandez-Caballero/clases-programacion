d803857860254f1f39e84e96a637b2bdda46b927
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-11-12 00:15:11 -0300

    FIX Nabar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import Navbar from './components/layouts/Navbar';
 import { navBarLinks } from './constants/navLinks';
 
@@ -6,7 +5,6 @@ function App(): JSX.Element {
 	return (
 		<div className='App'>
 			<Navbar navLinks={navBarLinks} />
-			<Button variant='contained'>Soy un material boton</Button>
 		</div>
 	);
 }
