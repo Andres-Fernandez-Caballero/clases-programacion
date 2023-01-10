747e310621831886e92e54c1a608001639584122
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-01-10 16:33:05 -0300

    REMOVE default margin in body html

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MainRouter from '@routes/MainRouter';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { CssBaseline } from '@mui/material';
 
 function App(): JSX.Element {
 	return (
 		<>
+			<CssBaseline />
 			<MainRouter />
 			<ToastContainer />
 		</>
