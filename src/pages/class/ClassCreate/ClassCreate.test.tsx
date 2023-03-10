533405e35ca6483ff34b8fd3b34277156944d576
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-03-10 14:11:02 -0300

    FIX build instance

--- src/pages/class/ClassCreate/ClassCreate.test.tsx
+++ src/pages/class/ClassCreate/ClassCreate.test.tsx
@@ -1,5 +1,5 @@
 import { cleanup, render } from '@testing-library/react';
-import ClassCreate from './ClassCreate';
+import { ClassCreate } from './ClassCreate';
 
 describe('ClassCreate', () => {
 	afterEach(cleanup);
---
+++ src/pages/class/ClassCreate/index.tsx
@@ -0,0 +1,22 @@
+import { ClassCreate } from '@pages/class/ClassCreate/ClassCreate';
+
+//
+// const ComponentCreateClass = () => {
+//
+//     const { classDto, resetClassDto, handleOnChange, handleChangeRange} = useClassCreateDto();
+//     const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+//         e.preventDefault();
+//         alert(classDto.programingLeanguage)
+//         resetClassDto();
+//     }
+//
+//     return (
+//         <ClassCreate
+//             onSubmit={handleOnSubmit}
+//             onChange={handleOnChange}
+//             onChangeRange={handleChangeRange}
+//         />
+//     )
+// }
+
+export default ClassCreate;
